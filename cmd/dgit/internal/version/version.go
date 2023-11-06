1be0c43cd37872cf4a694e8e00ff73969b6eae18
Daniel Moch <daniel@danielmoch.com>
2023-11-06 14:57:29 -0500

    dgit v0.1.0

--- cmd/dgit/internal/version/version.go
+++ cmd/dgit/internal/version/version.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	version = "0.1.1-dev0"
+	version = "0.1.0"
 
 	// Taken from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 	reString = `^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`
